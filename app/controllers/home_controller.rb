9e1550be1f9ccc45f9e06f7c39ce332f640a1e8d
Anh Huynh <huynhanh8327@gmail.com>
2022-11-02 00:31:00 -0700

    login success

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,19 +1,20 @@
 class HomeController < ApplicationController
   def index
     @user = User.new
-    
   end
 
   def login
     @user_login = User.find_by(username: params[:username], password: params[:password])
-    puts user_params
+
     respond_to do |format|
       if @user_login != nil
-        format.html { redirect_to login_path, notice: "login successful" }
-        format.json { render :show, status: :ok, location: @user }
+        format.html { redirect_to login_success_path, notice: "login successful" }
       else
         format.html { redirect_to root_path, notice: "login failed" }
       end
     end
+  end
+
+  def login_success
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   root 'home#index'
   get "/login", to: "home#login", as: "login"
+  get "/login_success", to: "home#login_success", as: "login_success"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
